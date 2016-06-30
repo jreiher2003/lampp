52603abfc131d7ae50f9b9709b33c020c9bb7881
Jeff Reiher <jreiher2003@yahoo.com>
2016-06-30 19:01:33 -0400

    Add .htaccess file

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+    return "Hello, I losssve Digital Ocean!"
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
